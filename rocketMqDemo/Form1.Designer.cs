856e28049f49fc228276753fdb8a39c3d590a716
李志强 <lizhiqiang1@msunsoft.com>
2019-11-09 19:41:09 +0800

    添加项目文件。

---
+++ rocketMqDemo/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace rocketMqDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtTopic = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGroup = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(48, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "socketMq NameServerAddr:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(365, 53);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(166, 21);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "10.68.5.196:9876";
+            // 
+            // txtTopic
+            // 
+            this.txtTopic.Location = new System.Drawing.Point(365, 97);
+            this.txtTopic.Name = "txtTopic";
+            this.txtTopic.Size = new System.Drawing.Size(166, 21);
+            this.txtTopic.TabIndex = 3;
+            this.txtTopic.Text = "topic1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(58, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "topic主题:";
+            // 
+            // txtGroup
+            // 
+            this.txtGroup.Location = new System.Drawing.Point(365, 134);
+            this.txtGroup.Name = "txtGroup";
+            this.txtGroup.Size = new System.Drawing.Size(166, 21);
+            this.txtGroup.TabIndex = 5;
+            this.txtGroup.Text = "group1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(73, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "group:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始消费消息";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(52, 273);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(627, 147);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "发布消息";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(285, 200);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(293, 21);
+            this.txtMsg.TabIndex = 9;
+            this.txtMsg.Text = "测试消息，哈哈";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(83, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "发布消息的内容:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtGroup);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTopic);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtTopic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGroup;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ rocketMqDemo/Form1.cs
@@ -0,0 +1,110 @@
+﻿using NewLife.Log;
+using NewLife.RocketMQ;
+using NewLife.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rocketMqDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                testConsumer();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void testConsumer()
+        {
+            var consumer = new Consumer
+            {
+                
+
+                Topic = txtTopic.Text,
+                Group = txtGroup.Text,
+                NameServerAddress = txtIP.Text,
+
+                FromLastOffset = false,
+                BatchSize = 20,
+                Log = XTrace.Log,
+            };
+
+            consumer.OnConsume = (q, ms) =>
+            {
+                log(string.Format("[{0}@{1}]收到消息[{2}]", q.BrokerName, q.QueueId, ms.Length));
+
+                foreach (var item in ms.ToList())
+                {
+                    log(string.Format($"消息：主键【{item.Keys}】，产生时间【{item.BornTimestamp.ToDateTime()}】，内容【{item.Body.ToStr()}】"));
+                }
+
+                return true;
+            };
+
+            consumer.Start();
+        }
+        private void testProduct()
+        {
+            var mq = new Producer
+            {
+              
+
+                Topic = txtTopic.Text,
+                NameServerAddress = txtIP.Text,
+                Group = txtGroup.Text
+                Log = XTrace.Log,
+            };
+            // 105命令的数字签名是 NyRea4g3OHmd7RxEUoVJUz58lXc=
+
+            mq.Start();
+
+           
+            var msg = new Msg();
+            msg.id = 1234;
+            msg.type = "order";
+            msg.content = txtMsg.Text;
+            mq.Publish(JsonHelper.ToJson(msg), "TagA");
+            mq.Dispose();
+        }
+        private void log(string msg)
+        {
+            this.Invoke(new Action(() => {
+                this.richTextBox1.AppendText(string.Format("时间:{0}\\r\\n内容:{1}\\r\\n", 
+                    DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), msg));
+            }));
+        }
+        public class Msg
+        {
+            public int id { get; set; }
+            public string type { get; set; }
+            public string content { get; set; }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                testProduct();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
